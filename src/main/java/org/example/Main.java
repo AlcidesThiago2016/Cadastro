72b2cb1a1a119729713f7f35ab3020000cfcc9eb
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-08-19 15:06:08 -0300

    Create class Usuario.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-            System.out.println("*****---SISTEMA DE CADASTRO---******");     }
-
+            System.out.println("*****---SISTEMA DE CADASTRO---******");
+    }
 }
